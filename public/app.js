06f9c301bfe263ecb974f724d739a0b9f2f093d8
SeanPrice13 <bentleyprice@live.com>
2022-05-13 10:22:59 -0400

    Added recommendations function.

--- public/app.js
+++ public/app.js
@@ -6,32 +6,35 @@ fetch("/api").then((response) => response.json()).then((data) => {
   checkFreq(data);
 });
 
-function countNum(numbers) {
+const countNum = (numbers) => {
   //Count the amount of times a number was drawn, regardless of position then display the result.
   const countThreshold = document.getElementById("count-slider").value;
   numbers.forEach((number) => {
-    document.getElementById("count").querySelectorAll("p").forEach((item) => {
-      const sliceA = item.innerText.slice(0, 2);
-      let sliceB = parseInt(item.firstElementChild.innerText);
-      if (number == sliceA) {
-        sliceB++;
-        item.firstElementChild.innerText = `${sliceB} times`;
-      }
-      if (sliceB !== 0) {
-        if (sliceB < countThreshold) {
-          item.classList.remove("high-count");
-          item.classList.add("low-count");
+    document
+      .getElementById("count")
+      .querySelectorAll("p")
+      .forEach((item) => {
+        const sliceA = item.innerText.slice(0, 2);
+        let sliceB = parseInt(item.firstElementChild.innerText);
+        if (number == sliceA) {
+          sliceB++;
+          item.firstElementChild.innerText = `${sliceB} times`;
         }
-        if (sliceB >= countThreshold) {
-          item.classList.remove("low-count");
-          item.classList.add("high-count");
+        if (sliceB !== 0) {
+          if (sliceB < countThreshold) {
+            item.classList.remove("high-count");
+            item.classList.add("low-count");
+          }
+          if (sliceB >= countThreshold) {
+            item.classList.remove("low-count");
+            item.classList.add("high-count");
+          }
         }
-      }
-    })
-  })
+      });
+  });
 };
 
-function checkFreq(numbers) {
+const checkFreq = (numbers) => {
   // Temporary variables to use in For Loops below.
   const freqThreshold = document.getElementById("freq-slider").value;
   // Number Position Arrays. (NPAs)
@@ -55,15 +58,15 @@ function checkFreq(numbers) {
     const arr = arrList[i];
     // From the number 1 to 28, count the amount of times each number appears in each NPA.
     for (let index = 1; index < 29; index++) {
-      let count = 0;
+      let fCount = 0;
       arr.forEach((element) => {
         if (index == element) {
-          count++;
+          fCount++;
         }
       });
       // Check if each NPA item's count is above or below the frequency threshold and push the item to the appropriate PFA.
-      if (count !== 0) {
-        count < freqThreshold
+      if (fCount !== 0) {
+        fCount < freqThreshold
           ? lowList[i].push(index)
           : highList[i].push(index);
       }
@@ -78,6 +81,33 @@ function checkFreq(numbers) {
   for (let l = 0; l < document.getElementById("low-freq").querySelectorAll("p").length; l++) {
     const row = document.getElementById("low-freq").querySelectorAll("p")[l].firstElementChild;
     row.append(lowList[l]);
+  }
+  recomGen(arrList);
+};
+
+const recomGen = (arrList) => {
+  const recomArr = [];
+  for (let index = 0; index < arrList.length; index++) {
+    const el = arrList[index],
+      highestNum = [],
+      highestCount = [];
+    const occurrences = el.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map());
+    // console.log([...occurrences.entries()]);
+    for (let i = 0; i < [...occurrences.entries()].length; i++) {
+      highestNum.push([...occurrences.entries()][i][0]);
+      highestCount.push([...occurrences.entries()][i][1]);
+    }
+    maxCount = Math.max(...highestCount);
+    maxIndex = highestCount.indexOf(maxCount);
+    maxNum = highestNum[maxIndex];
+    recomArr.push(maxNum);
+    console.log(recomArr);
+    console.log(`Highest number ${maxNum} with a count of ${maxCount} at index ${maxIndex}`);
+  }
+  for (let i = 0; i < document.getElementById('recom').querySelectorAll('p').length; i++) {
+    const el = document.getElementById('recom').querySelectorAll('p')[i];
+    el.innerText = '';
+    el.append(recomArr); 
   }
 };
 
