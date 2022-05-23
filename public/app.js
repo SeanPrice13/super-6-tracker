44ce0d21cf8530ef7a22b2d00f69c6cb2ffae606
SeanPrice13 <bentleyprice@live.com>
2022-05-23 18:00:50 -0400

    Refactored code & implemented date range feature

--- public/app.js
+++ public/app.js
@@ -1,29 +1,57 @@
+// import { allCombinations } from "./combinations.js";
+const countSlider = document.getElementById("count-slider"),
+  startDate = document.getElementById("start-date"),
+  endDate = document.getElementById("end-date");
+endDate.value = new Date().toISOString().slice(0, 10);
+
 /***************************************FUNCTIONS***************************************/
 // Fetch all draws from the database.
 fetch("/api").then((response) => response.json()).then((data) => {
-  window.dbNumbers = data;
-  countNum(dbNumbers);
+  dateRange(data);
 });
 
-const countNum = (numbers) => {
-  const countThreshold = document.getElementById("count-slider").value;
-  // Count the amount of times a number was drawn & store in a variable.
-  const occurrences = numbers.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map()),
-    [num, numCount] = [[...occurrences.keys()], [...occurrences.values()]];
+// Fetch draws within the specified range from the database.
+async function filterDatabase(fromDate, toDate) {
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({fromDate, toDate})
+  };
+  const response = await fetch('/api', options);
+  const data = await response.json();
+  dateRange(data);
+};
+
+function dateRange(db) {
+  const dateRangeArr = [];
+  for (let i = 0; i < db.length; i++) {
+    const data = db[i];
+    data.draw.forEach((num) => {
+      dateRangeArr.push(num);
+    });
+  };
+  countNum(dateRangeArr);
+};
+
+// Count the amount of times a number was drawn & store in a list.
+function countNum(numberArr) {
+  const countThreshold = countSlider.value;
+  const occurrences = numberArr.reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map()), [num, numCount] = [[...occurrences.keys()], [...occurrences.values()]];
   // Display the result in appropriate element.
   for (let i = 0; i < document.getElementById("count").querySelectorAll("p").length; i++) {
-    const el = document.getElementById("count").querySelectorAll("p")[i],
-      sliceB = num.findIndex((element) => element == el.innerText.slice(0, 2));
-    el.firstElementChild.innerText = `${numCount[sliceB]} times`;
-  // Apply CSS class based on count.
+    const el = document.getElementById("count").querySelectorAll("p")[i], sliceB = num.findIndex((element) => element == el.innerText.slice(0, 2));
+    el.firstElementChild.innerText = `${numCount[sliceB] || 0} times`;
+    // Apply CSS class based on count.
     parseInt(el.firstElementChild.innerText) >= countThreshold ? (el.classList.remove("low-count"), el.classList.add("high-count")) : (el.classList.remove("high-count"), el.classList.add("low-count"));
   }
   filterByFreq(num, numCount, countThreshold);
 };
 
-const filterByFreq = (unfilteredArr, unfilteredCount, threshold) => {
+// Filter numbers to filteredList if number count is above threshold.
+function filterByFreq(unfilteredArr, unfilteredCount, threshold) {
   const filteredList = [];
-  // Filter numbers to filteredList if number count is above threshold.
   for (let i = 0; i < unfilteredCount.length; i++) {
     const el = unfilteredCount[i];
     if (el >= threshold) {
@@ -33,39 +61,29 @@ const filterByFreq = (unfilteredArr, unfilteredCount, threshold) => {
   recomGen(filteredList);
 };
 
-const recomGen = (filteredArray) => {
-  // Display suggested draws based on filteredArray.
+// Display suggested draws based on filteredArray.
+function recomGen(filteredArray) {
   for (let i = 0; i < document.getElementById('recom').querySelectorAll('p').length; i++) {
     const el = document.getElementById('recom').querySelectorAll('p')[i];
     el.innerText = '';
-    el.append((filteredArray.sort(() => Math.random() - Math.random()).slice(0, 6)).sort()); 
+    el.append((filteredArray.sort(() => Math.random() - Math.random()).slice(0, 6)).sort());
   }
 };
 
 /***************************************Event Listeners***************************************/
-// Count Threshold Slider Event Listener
-document.getElementById("count-slider").addEventListener("change", () => {
-  document.getElementById("count").querySelectorAll("p").forEach((p) => {
-    p.firstElementChild.innerText = "0 times";
+// Count Threshold, Date Range Event Listeners in a for loop
+[countSlider, startDate, endDate].forEach((item) => {
+  item.addEventListener('change', () => {
+    filterDatabase(startDate.value.replace(/-/g, ""), endDate.value.replace(/-/g, ""));
   });
-  countNum(dbNumbers);
 });
+
 // Refresh Button Event Listener
-document.getElementById("refresh-btn").addEventListener("click", () => {
-  location.reload();
+document.getElementById("refresh-db-btn").addEventListener("click", () => {
+  document.getElementById("title").textContent = 'Updating Database.';
+  fetch("/scrape").then((response) => response.json()).then((data) => {
+    document.getElementById('title').textContent = data;
+  });
 });
 
 /***************************************Test Code***************************************/
-
-// async function postDatabase(drawData) {
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify(drawData)
-// };
-// const response = await fetch('/api', options);
-// const data = await response.json();
-//     console.log(data);
-// }
--- scraper.js
+++ scraper.js
@@ -1,101 +1,85 @@
 // My First Node.js server with Express & NEDB!
-const drawData = [];
 const express = require("express");
 const puppeteer = require("puppeteer");
 const cors = require("cors");
-const Datastore = require("nedb");
 const app = express();
+const Datastore = require("nedb");
 const database = new Datastore({ filename: "database.db", autoload: true });
 
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 app.use(cors());
 
-app.get("/api", async (req, res) => {
+// Scrape the NLA Super 6 data and add to database.
+app.get("/scrape", async (req, res) => {
   try {
-    // Scrape the NLA Super 6 data.
     const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.goto("https://www.nla.gd/wp-json/wp/v2/pages/549");
     const el = await page.$eval("pre", (element) => element.textContent);
     browser.close();
     formatRawNLAData(el);
-    addDrawToDb();
-    allDbDrawNumbers(res);
+    res.json('Done. Refresh Page.');
   } catch (e) {
-    console.log(e);
     res.status(500).send(e);
   }
+});
+
+// Send all database draw data to the user.
+app.get("/api", async (req, res) => {
+  try {
+    database.find({}, (err, result) => {
+      if (err) console.log(err);
+      res.json(result);
+    });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Pull draws within requested date from database & send to client.
+app.post('/api', (req, res) => {
+  const draws = [];
+  database.find({$and: [{date: {$gte: req.body.fromDate}}, {date: {$lte: req.body.toDate}}]}, (err, result) => {
+    if (err) console.log(err);
+    for (let i = 0; i < result.length; i++) {
+      const draw = result[i];
+      draws.push(draw);
+    }
+    res.json(draws);
+  });
 });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Running on port 5000.");
 });
 
-// app.post('/api', (req, res) => {
-//     console.time('post2db');
-//     console.log('I got a request!');
-//     // console.log(req.body.drawData);
-//     database.insert(req.body.drawData);
-//     res.json(req.body.drawData[0].key);
-//     console.timeEnd('post2db');
-// });
-
-const formatRawNLAData = (el) => {
-  // Format the scraped data for use.
-  const raw = JSON.parse(el).content.rendered.slice(JSON.parse(el).content.rendered.indexOf("<table><thead>")).toString().replace(/(<([^>]+)>)/gi, "").replace("DateWinning#LetterDraw ID", "").replace(/[,-\s]/g, "");
-  let dS1 = 0,
-    dS2 = 8,
-    nS2 = 20,
-    lS2 = 21,
-    iS2 = 25;
-  for (let i = 0; i < raw.length / 25; i++) {
-    const drawDate = raw.slice(dS1, dS2);
-    const drawNums = raw.slice(dS2, nS2).match(/.{1,2}/g);
-    const drawLtr = raw.slice(nS2, lS2);
-    const drawId = raw.slice(lS2, iS2);
-    drawData.push({
-      date: drawDate,
-      draw: drawNums,
-      letter: drawLtr,
-      key: drawId,
-    });
+/***************************************FUNCTIONS***************************************/
+// Format the scraped data into individual draws and push to drawData array.
+function formatRawNLAData(rawScrapedData) {
+  const drawData = [];
+  const rawFilteredData = JSON.parse(rawScrapedData).content.rendered.slice(JSON.parse(rawScrapedData).content.rendered.indexOf("<table><thead>")).toString().replace(/(<([^>]+)>)/gi, "").replace("DateWinning#LetterDraw ID", "").replace(/[,-\s]/g, "");
+  let dS1 = 0, dS2 = 8, nS2 = 20, lS2 = 21, iS2 = 25;
+  for (let i = 0; i < rawFilteredData.length / 25; i++) {
+    drawData.push({date: rawFilteredData.slice(dS1, dS2), draw: rawFilteredData.slice(dS2, nS2).match(/.{1,2}/g), letter: rawFilteredData.slice(nS2, lS2), key: rawFilteredData.slice(lS2, iS2)});
     dS1 += 25;
     dS2 += 25;
     nS2 += 25;
     lS2 += 25;
     iS2 += 25;
   }
+  addDrawToDb(drawData);
 };
 
-const addDrawToDb = () => {
-  // Check database for most recent draw & add to database if not found.
-  for (let j = 0; j < drawData.length; j++) {
-    const iD = drawData[j].key;
+// Check database for each recent draw & add to database if not found.
+function addDrawToDb(drawArr) {
+  for (let i = 0; i < drawArr.length; i++) {
+    const iD = drawArr[i].key;
     database.findOne({ key: iD }, (err, output) => {
-      if (err) {
-        console.log(err);
-      }
-      output !== null ? 
-      (output.key == iD ? console.log("EXISTS!", iD) : console.log()) : 
-      (console.log(output, iD), database.insert(drawData[j]), console.log("ADDED!", iD));
+      if (err) console.log(err);
+      output !== null ?
+        (output.key == iD ? console.log("EXISTS!", iD) : console.log()) :
+        (console.log(output, iD), database.insert(drawData[i]), console.log("ADDED!", iD));
     });
   }
-};
-
-const allDbDrawNumbers = (res) => {
-  // Pull all draw numbers from database & send to client.
-  const numbers = [];
-  database.find({}, (err, result) => {
-    if (err) {
-      console.log(err);
-    }
-    for (let k = 0; k < result.length; k++) {
-      const draw = result[k];
-      draw.draw.forEach((num) => {
-        numbers.push(num);
-      });
-    }
-    res.json(numbers);
-  });
 };
